7122a9e7d3d372a72d1e736683c97c52832098c5
Gogus91 <agnieszkadopieralska53@gmail.com>
2018-08-25 16:49:31 +0200

    My Django Girls app, first commit

---
+++ Django_day_1.py
@@ -0,0 +1,20 @@
+
+
+# liczba = 5
+#
+# while liczba < 10:
+#     liczba += 1
+#     print (liczba)
+
+
+# dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+#
+# def hi(imie):
+#     print('Witaj ' + imie + '!')
+#
+# for imie in dziewczyny:
+#     hi(imie)
+#     print('Kolejna dziewczyna')
+
+for i in range (1,6):
+    print(i)
